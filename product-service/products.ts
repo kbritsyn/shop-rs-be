98c32a81897ad43ab75311903579ae2475bd36fc
kbritsyn <britsyn.kirill@outlook.com>
2020-11-02 00:53:16 +0400

    Small update to products data

--- product-service/products.ts
+++ product-service/products.ts
@@ -4,41 +4,41 @@ export const products = [
     count: 4,
     description: 'Short Product Description1',
     price: 2.4,
-    title: 'Product1'
+    title: 'New Product1'
   },
   {
     id: 2,
     count: 1,
     description: 'Short Product Description2',
     price: 21.4,
-    title: 'Product2'
+    title: 'New Product2'
   },
   {
     id: 3,
     count: 1,
     description: 'Short Product Description3',
     price: 21.4,
-    title: 'Product3'
+    title: 'New Product3'
   },
   {
     id: 4,
     count: 1,
     description: 'Short Product Description4',
     price: 41.4,
-    title: 'Product4'
+    title: 'New Product4'
   },
   {
     id: 5,
     count: 1,
     description: 'Short Product Description5',
     price: 51.4,
-    title: 'Product5'
+    title: 'New Product5'
   },
   {
     id: 6,
     count: 1,
     description: 'Short Product Description6',
     price: 61.4,
-    title: 'Product6'
+    title: 'New Product6'
   }
 ]
