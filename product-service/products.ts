c583179ccafd60c907009ce632446e5ee6fcf07a
kbritsyn <britsyn.kirill@outlook.com>
2020-11-02 00:37:18 +0400

    Updated products list

--- product-service/products.ts
+++ product-service/products.ts
@@ -1,22 +1,44 @@
 export const products = [
   {
     id: 1,
-    name: 'Test 1'
+    count: 4,
+    description: 'Short Product Description1',
+    price: 2.4,
+    title: 'Product1'
   },
   {
     id: 2,
-    name: 'Test 2'
+    count: 1,
+    description: 'Short Product Description2',
+    price: 21.4,
+    title: 'Product2'
   },
   {
     id: 3,
-    name: 'Test 3'
+    count: 1,
+    description: 'Short Product Description3',
+    price: 21.4,
+    title: 'Product3'
   },
   {
     id: 4,
-    name: 'Test 4'
+    count: 1,
+    description: 'Short Product Description4',
+    price: 41.4,
+    title: 'Product4'
   },
   {
     id: 5,
-    name: 'Test 5'
+    count: 1,
+    description: 'Short Product Description5',
+    price: 51.4,
+    title: 'Product5'
   },
+  {
+    id: 6,
+    count: 1,
+    description: 'Short Product Description6',
+    price: 61.4,
+    title: 'Product6'
+  }
 ]
