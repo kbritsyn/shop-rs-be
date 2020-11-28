093a5f93fd101a2ab2ea43b365253ef7942dbcbb
kbritsyn <britsyn.kirill@outlook.com>
2020-11-28 12:11:40 +0400

    Added awaiting for saving products and sending notifications in catalogBatchProcess lambda

--- product-service/handlers/catalog-batch-process.ts
+++ product-service/handlers/catalog-batch-process.ts
@@ -3,32 +3,27 @@ import { SQSEvent, SQSHandler } from 'aws-lambda';
 import { SNS } from 'aws-sdk';
 import { insertProduct } from '../db/insert-product';
 
-export const catalogBatchProcess: SQSHandler = (event: SQSEvent): void => {
+export const catalogBatchProcess: SQSHandler = async (event: SQSEvent): Promise<void> => {
   console.log('Lambda invocation with event: ', event);
 
   const sns = new SNS({ region: 'eu-west-1' });
   const products: ProductDTO[] = event.Records.map(({ body }) => JSON.parse(body));
 
   try {
-    products.forEach(async (p) => {
-      await insertProduct(p);
-    });
-    publishNotification(sns, products);
+    await Promise.all(products.map(async (product) => insertProduct(product)));
+    console.log('Products were saved to database');
+
+    await publishNotification(sns, products);
+    console.log('Notification sent');
   } catch (error) {
-    console.error('Error in batch processing: ', error);
+    console.log('Error in batch processing: ', error);
   }
 }
 
 function publishNotification(sns: SNS, products: ProductDTO[]) {
-  sns.publish({
+  return sns.publish({
     TopicArn: process.env.SNS_ARN,
     Subject: 'New products have been added',
     Message: JSON.stringify(products)
-  }, err => {
-    if (err) {
-      console.error('Failed to send SNS notification: ', err);
-      return;
-    }
-    console.log('Notification sent');
-  });
+  }).promise();
 }
